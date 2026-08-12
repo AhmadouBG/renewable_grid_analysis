ed122e5d2c32a002421232eb049c7563c4d591d3
AhmadouBG <gueyebamba39@gmail.com>
2026-08-12 13:47:01 +0000

    feat: add staging raw hourly

---
+++ dbt_weather_data_analysis/models/staging/stg_hourly.sql
@@ -0,0 +1,24 @@
+SELECT
+    timestamp,
+    arrondissement_name,
+    point_grid_id,
+    latitude,
+    longitude,
+    temperature_2m,
+    cloud_cover_pct,
+    direct_radiation_w_m2,
+    shortwave_radiation_w_m2,
+    wind_speed_80m_kmh,
+    wind_direction_80m_deg,
+    precipitation_mm,
+    visibility_m,
+    visibility_m / 1000.0 AS visibility_km,
+    weather_code_wmo,
+    CASE
+        WHEN shortwave_radiation_w_m2 >= {{var ('radiation_sunny_threshold')}} THEN 'Ensoleillé'
+        WHEN shortwave_radiation_w_m2 < {{var ('radiation_cloudy_threshold')}} THEN 'Non Ensoleillé'
+        ELSE 'Éclaircies / Mixte'
+    END AS etat_ensoleillement,
+    visibility_m / 1000.0 <= {{var ('visibility_alert_km')}} AS alerte_visibilite
+FROM
+    {{source ('raw', 'raw_hourly')}}
